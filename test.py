f2fb059d7c5b1ffa404f293aced9b99097439847
Jeffrey Yan <chameleon6@gmail.com>
2015-11-18 17:31:26 -0500

    better chunking

--- test.py
+++ test.py
@@ -1,10 +1,19 @@
+import sys
 import scipy.io.wavfile
 import numpy as np
 import matplotlib.pyplot as plt
 import cPickle as pickle
 import random
+from sklearn.decomposition import PCA
+
+USE_PCA = True
+DEBUG = True
 
 def cache_or_compute(fname, fun, *args):
+    if DEBUG:
+        c = fun(*args)
+        np.save(fname, c)
+        return c
     try:
         return np.load(fname)
     except IOError:
@@ -14,41 +23,59 @@ def cache_or_compute(fname, fun, *args):
 
 # np.set_printoptions(threshold=np.nan)
 
-rate, data = scipy.io.wavfile.read("long_test.wav")
-keypress_len = 5000
+rate, data = scipy.io.wavfile.read("long_test_2.wav")
+keypress_first_filter_len = 3000
+max_keypress_len = 10000
+min_keypress_len = 5000
 
 # the fraction of the file we use
 file_range = (0, 1.0)
 
-def get_cluster_starts(data):
+def stft(x, fs=44100, framesz=0.01, hop=0.005):
+    framesamp = int(framesz*fs)
+    hopsamp = int(hop*fs)
+    w = scipy.hanning(framesamp)
+    X = scipy.array([scipy.fft(w*x[i:i+framesamp])
+                     for i in range(0, len(x)-framesamp, hopsamp)])
+    return np.sqrt(np.sum(np.absolute(X)**2, axis=1)), X
+
+def get_chunk_starts(data):
     thresh = sorted(data)[int(0.99 * len(data))]
     is_start = data > thresh
     for (i,v) in enumerate(is_start):
         if v:
-            for j in range(i+1, min(len(data), i+keypress_len)):
+            for j in range(i+1, min(len(data), i+keypress_first_filter_len)):
                 is_start[j] = False
 
     current_chunk = []
     data_chunks = []
+    last_max = 0
     for (b, d) in zip(is_start, data):
         current_chunk.append(d)
         if b:
-            data_chunks.append(np.array(current_chunk))
+            current_max = max(current_chunk)
+            if (current_max > last_max and
+                    (len(data_chunks) == 0 or len(data_chunks[-1]) > min_keypress_len)) \
+                    or len(data_chunks[-1]) > max_keypress_len:
+                data_chunks.append(np.array(current_chunk))
+            else:
+                data_chunks[-1] = np.append(data_chunks[-1],np.array(current_chunk))
             current_chunk = []
+            last_max = current_max
 
     return data, is_start, np.array(data_chunks)
 
 def get_features(chunk):
     '''Grab the features from a single keystroke'''
     # The definition of cepstrum
-    # chunk = chunk[0:-500]
-    # max_ind = np.argmax(chunk)
-    # if max_ind < 0.2 * len(chunk):
-    #     chunk = chunk[max_ind:]
+    chunk = chunk[0:-500]
+    max_ind = np.argmax(chunk)
+    if max_ind < 0.2 * len(chunk):
+        chunk = chunk[max_ind:]
     f=np.absolute(np.fft.fft(chunk, n=500))
-    lf = np.log( f ** 2 + 0.0001)
+    lf = np.log(f ** 2 + 0.0001)
     c = (np.absolute(np.fft.ifft(lf))**2)
-    return c[1:50]
+    return c[10:len(c)/2]
 
 def clusterize(ls, n=50):
     '''Clusters the objects (np arrays) in ls into n clusters
@@ -99,28 +126,41 @@ def clusterize(ls, n=50):
     return np.array([get_closest_cluster(means, l) for l in ls]), means
 
 chunks = cache_or_compute(
-    'cache/chunks.npy', lambda arg: get_cluster_starts(arg)[2],
+    'cache/chunks.npy', lambda arg: get_chunk_starts(arg)[2],
     data[int(file_range[0] * len(data)): int(file_range[1] * len(data))])
 
 chunk_lens = [len(c) for c in chunks]
+chunk_maxs = [max(c) for c in chunks]
 inds = []
 last_ind = 0
 for l in chunk_lens:
     last_ind += l
     inds.append(last_ind)
 
-"""
+print "num_chunks", len(chunks)
 # segmentation plot
-N = 2000000
-M = 2200000
+N = 0
+M = 500000
 ii = np.zeros(len(data))
-ii[inds] = 1000
+ii[inds] = 1
+#ft_data, raw = stft(data[N:M])
+#plt.plot(ft_data/np.max(ft_data))
+#plt.plot(data[N:M:221]/float(np.max(data[N:M]))*4, "r")
 plt.plot(data[N:M])
-plt.plot(ii[N:M])
+plt.plot(max(data[N:M]) * ii[N:M], "r")
+plt.plot(inds[0:50], chunk_maxs[0:50])
 plt.show()
-"""
+
+sys.exit()
 
 features = cache_or_compute('cache/features.npy', lambda ls: map(get_features, ls), chunks)
+
+if USE_PCA:
+    pca = PCA(n_components=20)
+    pca.fit(features)
+    print pca.explained_variance_ratio_
+    features = pca.transform(features)
+
 print features[0]
 print features[1]
 print features[2]
@@ -138,6 +178,8 @@ for c in clusters:
 rel_freqs = {}
 for c in freqs:
     rel_freqs[c] = float(freqs[c])/n_clusters
+
+#sys.exit()
 
 #plt.bar([i[0] for i in freqs.items()], [i[1] for i in freqs.items()])
 #plt.show()
@@ -223,7 +265,8 @@ cache_or_compute("cache/next_letter_prob.npy", compute_bigram)
 pi, A = np.load("cache/next_letter_prob.npy").tolist()
 valid_letters = map(chr, range(97,123)) + [' ']
 n_letters = len(valid_letters)
-letters_typed = [valid_letters[int(n_letters * random.random())] for i in range(n_clusters)]
+letters_typed = ['t','h','e',' ']  + [valid_letters[int(n_letters * random.random())] for i in range(n_clusters-4)]
+#letters_typed = [random.choice(['e','t','r','a','s',' ']) for i in range(n_clusters)]
 eta = {}
 most_likely_seq = {}
 best_probs = {}
