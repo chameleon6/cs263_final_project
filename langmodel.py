aaa6a00b4c60895730467d0fb1f9b911b7106f33
David Ding <davidding2000@gmail.com>
2015-11-25 14:40:05 -0800

    code cleanup

---
+++ langmodel.py
@@ -0,0 +1,55 @@
+import re
+import numpy as np
+
+valid_letters = map(chr, range(97,123)) + [' ']
+def compute_freq_dist(data):
+    counts = {}
+    freq = {}
+    outputs = {}
+
+    for i, o in data:
+        if i not in counts:
+            counts[i] = 0
+            freq[i] = {}
+        if o not in freq[i]:
+            freq[i][o] = 0
+        freq[i][o] += 1
+        counts[i] += 1
+        outputs[o] = True
+
+    for i in freq:
+        for o in outputs:
+            if o not in freq[i]:
+                freq[i][o] = 0
+
+    for i in freq:
+        for o in freq[i]:
+            freq[i][o] /= float(counts[i])
+
+    for c in counts:
+        counts[c] /= float(sum(counts.values()))
+
+    return counts, freq
+
+def compute_bigram():
+    try:
+        return np.loads('cache/next_letter_probs.npy')
+    except Exception as e:
+        next_letter_prob = {}
+
+        with open("full_text.txt", "r") as f:
+            s = f.read().lower()
+            s = re.sub('[^a-z ]', ' ', s)
+            s = re.sub(' +', ' ', s)
+            letter_pairs = np.array(zip(s, s[1:]))
+            pi, A = compute_freq_dist(letter_pairs)
+
+            pi_v = np.zeros(len(valid_letters))
+            theta_v = np.zeros((len(valid_letters), len(valid_letters)))
+
+            for i, c in enumerate(valid_letters):
+                pi_v[i] = pi[c]
+                for j, c2 in enumerate(valid_letters):
+                    theta_v[i, j] = A[c][c2]
+            np.save('cache/next_letter_prob.npy', (pi, A, pi_v, theta_v))
+            return pi, A, pi_v, theta_v
--- test.py
+++ test.py
@@ -19,6 +19,9 @@ from mlalgs import (
     longest_common_subseq
 )
 
+from langmodel import compute_bigram
+pi, A, pi_v, theta_v = compute_bigram()
+
 ###################################################################
 # Constants and debugging control
 ###################################################################
@@ -235,62 +238,6 @@ if CURRENT_STAGE == 'Clustering':
 ###################################################################
 # HMM Coefficient computation
 ###################################################################
-
-def compute_freq_dist(data):
-    counts = {}
-    freq = {}
-    outputs = {}
-
-    for i, o in data:
-        if i not in counts:
-            counts[i] = 0
-            freq[i] = {}
-        if o not in freq[i]:
-            freq[i][o] = 0
-        freq[i][o] += 1
-        counts[i] += 1
-        outputs[o] = True
-
-    for i in freq:
-        for o in outputs:
-            if o not in freq[i]:
-                freq[i][o] = 0
-
-    for i in freq:
-        for o in freq[i]:
-            freq[i][o] /= float(counts[i])
-
-    for c in counts:
-        counts[c] /= float(sum(counts.values()))
-
-    return counts, freq
-
-def compute_bigram():
-    next_letter_prob = {}
-
-    valid_letters = map(chr, range(97,123)) + [' ']
-    with open("full_text.txt", "r") as f:
-        s = f.read().lower()
-        s = re.sub('[^a-z ]', ' ', s)
-        s = re.sub(' +', ' ', s)
-        letter_pairs = np.array(zip(s, s[1:]))
-        return compute_freq_dist(letter_pairs)
-
-pi, A = cache_or_compute("cache/next_letter_prob.npy", compute_bigram)
-
-###################################################################
-# HMM Viterbi
-###################################################################
-
-valid_letters = map(chr, range(97,123)) + [' ']
-
-pi_v = np.zeros(len(valid_letters))
-theta_v = np.zeros((len(valid_letters), len(valid_letters)))
-
-for i, c in enumerate(valid_letters):
-    pi_v[i] = pi[c]
-    for j, c2 in enumerate(valid_letters):
-        theta_v[i, j] = A[c][c2]
 
 spaces = [c for s, c in zip(text, clusters) if s == ' ']
 baum_welch(pi_v, theta_v, clusters, spaces)
